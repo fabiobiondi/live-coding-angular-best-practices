c25c15dfe2f329f881bdd5841fa982c9cd3878ff
Fabio <info@fabiobiondi.com>
2019-05-02 17:21:15 +0200

    Remove useless elements in NavBar

--- src/app/core/components/navbar.component.ts
+++ src/app/core/components/navbar.component.ts
@@ -5,25 +5,23 @@ import { Component } from '@angular/core';
   template: `
     <nav class="navbar navbar-expand navbar-dark bg-dark text-white">
       <a class="navbar-brand">LOGO</a>
-      <div class="navbar-collapse collapse">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link"
-              routerLink="/login"
-              routerLinkActive="active">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" 
-               routerLinkActive="active" 
-               routerLink="/catalog">Catalog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link"
-               routerLinkActive="active"
-               routerLink="/users">users</a>
-          </li>
-        </ul>
-      </div>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link"
+            routerLink="/login"
+            routerLinkActive="active">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" 
+             routerLinkActive="active" 
+             routerLink="/catalog">Catalog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"
+             routerLinkActive="active"
+             routerLink="/users">Users</a>
+        </li>
+      </ul>
     </nav>
   `,
   styles: []
